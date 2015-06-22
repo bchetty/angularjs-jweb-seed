267101c27a67acace06bbca5c22655f95a7b354c
Babji Prashanth, Chetty <dev.b.chetty@gmail.com>
2015-06-22 21:02:06 +0200

    new changes
    new changes

--- src/main/webapp/scripts/controllers/myAppControllers.js
+++ src/main/webapp/scripts/controllers/myAppControllers.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('tsApp.controllers', [])
+angular.module('myApp.controllers', [])
         .controller('homeController', function ($scope) {
             $scope.message = "HOME PAGE!";    
         })
